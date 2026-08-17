b2570899b8642415708bc0e014e34850abb6f391
Samuel Angulo Meza <sangulom@unicesar.edu.co>
2026-08-17 15:48:44 -0500

    feat: implementar READ de Libro (listar)

--- Taller1-Biblioteca-Git/src/main/java/com/mycompany/taller1/biblioteca/git/Main.java
+++ Taller1-Biblioteca-Git/src/main/java/com/mycompany/taller1/biblioteca/git/Main.java
@@ -120,5 +120,18 @@ public class Main {
 
         System.out.println("Libro creado correctamente.");
     }
+    public static void listarLibros() {
+
+        System.out.println("\n--- LISTA DE LIBROS ---");
+
+        if (libros.isEmpty()) {
+            System.out.println("No hay libros registrados.");
+            return;
+        }
+
+        for (Libro libro : libros) {
+            System.out.println(libro);
+        }
+    }
     
 }
